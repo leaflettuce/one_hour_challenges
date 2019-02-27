b4f530242260778d7ed70f56b0bc35a9e12b677a
Andrew Trick <andyjtrick@gmail.com>
2019-02-27 09:52:32 -0600

    first commit & first challenge done.

---
+++ ONE-audit_cluster/audit_clustering.R
@@ -0,0 +1,121 @@
+library(rattle)
+library(ggplot2)
+df <-audit
+
+#######
+# EDA #
+#######
+str(df)
+
+# remove unneeded cols
+df$ID <- NULL
+df$RISK_Adjustment <- NULL
+df$TARGET_Adjusted <- NULL
+df$Education <- NULL
+df$Deductions <- NULL
+df$Employment <-  NULL
+df$IGNORE_Accounts <- NULL
+
+# quick plots
+hist(df$Age)
+plot(df$Gender)
+plot(df$Marital)
+
+ggplot(aes(x = Education), data  = df) + 
+  geom_histogram(stat = "count")
+
+ggplot(aes(x = Occupation), data  = df) + 
+  geom_histogram(stat = "count")
+
+ggplot(aes(x = Income), data = df) + 
+  geom_histogram()
+
+hist(df$Hours)
+
+ggplot(aes(x = Age, y = Income, color = Gender), data = df) +
+  geom_jitter(size = Hours*.05)
+
+############
+# Cleaning #
+############
+
+# limit outliers
+df$Income <- ifelse(df$Income > 150000, 150000, df$Income)
+
+# dummy out factors
+sum(is.na(df$Gender)) 
+df$Female <- ifelse(df$Gender == "Female", 1, 0)
+df$Gender <- NULL
+
+# marriage status
+df$Married <- ifelse(df$Marital == "Married" |
+                       df$Marital == "Married-spouse-absent", 1, 0)
+
+df$Single <- ifelse(df$Marital == "Divorced" |
+                      df$Marital == "Unmarried" |
+                      df$Marital == "Widowed", 1, 0)
+
+df$absent <- ifelse(df$Marital == "Absent", 1, 0)
+
+df$Marital <- NULL
+
+# remove na
+df = na.omit(df)
+
+
+#############
+# normalize #
+#############
+
+min_max_norm <- function(x) {
+  return((x - min(x))/(max(x) - min(x)))
+}
+
+df[ , c(1,3, 4)] <- lapply(df[ , c(1, 3, 4)], min_max_norm)
+
+
+occupation <- df$Occupation
+df$Occupation <- NULL
+
+str(df)
+
+###########
+# Cluster #
+###########
+
+# k-means
+library(stats)
+
+clusters <- kmeans(df, 7)
+
+# examine
+clusters$centers
+clusters$size
+
+df$Cluster_id <- clusters$cluster
+
+
+# visualize
+
+ggplot(aes(x = Age, y = Income, color = as.factor(Cluster_id), 
+           shape = as.factor(Female)), data = df) + 
+          geom_jitter(size = 2)
+  
+
+# split test -train
+train <- df[1:1400, ]
+test <- df[1401:1899, ]
+
+y_train <- occupation[1:1400]
+y_test <- occupation[1401:1899]
+
+# k-nn predict occupation
+library(class)
+library(gmodels)
+
+pred <- knn(train = train, test = test, cl = y_train, k = 5)
+
+CrossTable(x = y_test, y = pred, prop.chisq = FALSE)$prop.r
+
+head(pred, 8)
+head(y_test, 8)
---
+++ README.md
@@ -0,0 +1,23 @@
+# One Hour Challenges
+### Quick burst challenge projects
+Different projects I attempt with a one hour time limit. I'll set either a goal or library/methodology I want to use and attempt to complete the project within an hour.
+
+
+#### ONE - Semi Supervised Learning
+```
+GOAL: See if I can predict a person's occupation by different demographic statistics
+
+METHOD: 
+	- I use the audit data provided by Rattle to pull demographic and occupation data
+	- Quick cleaning of the data into form suited for models to be used
+	- Cluster the data using k-means 
+	- Imputer clusters into dataset as additional input for inference
+	- Classify using k-nn to predict occupation
+	
+RESULTS: 
+	- Clustering results display defined segmentation between the give inputs (age, income, marital, gender).
+	- Particularly defined groups at either 3, 5, and 7 k.
+	- Using a k = sqrt(col_len) results in a 'decent' predictor for k-nn.
+	- Too many occupation possibilities and results are overall weak.
+	- On average accurate predicting an occupation at around ~25%
+```
