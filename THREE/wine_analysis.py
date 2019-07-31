5636bb0bd65a2da5352953993e416ccdc4df7bc6
Andrew Trick <andyjtrick@gmail.com>
2019-07-31 10:35:06 -0500

    prep data.

--- THREE/wine_analysis.py
+++ THREE/wine_analysis.py
@@ -6,6 +6,7 @@ Created on Wed Jul 31 10:13:48 2019
 """
 
 import pandas as pd
+import numpy as np
 import os
 
 ##########################
@@ -15,4 +16,20 @@ import os
 # setwd
 os.chdir('E:/projects/one_hour_challenges/THREE')
 # import
-df = pd.read_csv('../data/wine_reviews.csv')
+df = pd.read_csv('../data/wine_reviews.csv', index_col = 0)
+
+# understand df
+pd.set_option('display.max_columns', None)
+df.shape
+df.describe()
+df.head()
+df.info()
+df.columns
+df.values
+
+# remove NA's
+df.isnull().sum(axis = 0)
+
+df = df[np.isfinite(df['price'])]
+df = df[np.isfinite(df['points'])]
+
